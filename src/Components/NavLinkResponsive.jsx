f670fd7485fb65a0bd6730338095921747e5df20
eddievasquez2110 <eddieomarvasquez@gmail.com>
2023-07-29 22:36:52 -0500

    componentes

--- src/Components/NavLinkResponsive.jsx
+++ src/Components/NavLinkResponsive.jsx
@@ -9,8 +9,8 @@ export default function NavLinkResponsive({ method = 'get', as = 'a', to, active
             to={to}
             className={`w-full flex items-start pl-3 pr-4 py-2 border-l-4 ${
                 active
-                    ? 'border-green-300 text-green-300  border-green-300 focus:outline-none focus:text-green-300 focus:border-green-300 focus:border-greeb-300'
-                    : 'border-transparent text-black hover:text-gray-800 hover:bg-green-100 hover:border-green-300'
+                    ? 'border-indigo-500/100 text-green-300  border-indigo-500/100 focus:outline-none focus:text-indigo-500/100 focus:border-indigo-400/100 focus:border-indigo-200/100'
+                    : 'border-transparent text-black hover:text-gray-800 hover:bg-indigo-200/100 hover:border-indigo-400/100'
             } text-base font-medium focus:outline-none transition duration-150 ease-in-out`}
         >
             {children}
