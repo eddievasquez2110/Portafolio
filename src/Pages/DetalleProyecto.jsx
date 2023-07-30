22e385c10fca8536de663cf51bc287093ef6c128
eddievasquez2110 <eddieomarvasquez@gmail.com>
2023-07-30 12:18:39 -0500

    Paginas

--- src/Pages/DetalleProyecto.jsx
+++ src/Pages/DetalleProyecto.jsx
@@ -20,16 +20,16 @@ const DetalleProyecto = () => {
         {
             proyectos.map(proy => proy.id == id ? 
                 
-                <div className="contenedor_detproyectos rounded-lg shadow-lg" key={proy.id}>
-                <div className="titulo overflow-hidden bg-black rounded-t-lg">
-                    <h2 className="">
+                <div className="contenedor_detproyectos rounded-lg shadow-lg " key={proy.id}>
+                <div className="titulo overflow-hidden bg-black rounded-t-lg mt-10 md:mt-0">
+                    <h2 className="text-xl md:text-2xl">
                         {proy.titulo}
                     </h2>
                 </div>
-                <div className="flex flex-row p-10 justify-evenly gap-10">
-                    <img className="h-80 w-2/4 shadow-lg shadow-blue-200 rounded-lg" src={proy.imagen} />
-                    <div className="flex flex-col w-2/4">
-                        <Link to={proy.demo} className="md:w-30 flex items-center justify-center gap-3 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 rounded-lg mt-10 hover:bg-indigo-500 transition ease-in-out duration-300">
+                <div className="contendor_detproyectos_items flex flex-col md:flex-row p-5 pt-10 md:p-10 justify-evenly md:gap-10">
+                    <img className="h-80 w-full md:w-2/5 shadow-lg shadow-blue-200 rounded-lg xs:pt-5" src={proy.imagen} />
+                    <div className="flex flex-col w-full md:w-2/4">
+                        <Link to={proy.demo} className=" flex items-center justify-center gap-3 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 rounded-lg mt-10 hover:bg-indigo-500 transition ease-in-out duration-300">
                             <FaPlayCircle className="icon1" />Visualizar Demo
                         </Link>
                         <Link to={proy.link} target="_blank" className="md:w-30 flex items-center justify-center gap-3 bg-indigo-600 hover:bg-blue-dark text-white font-bold py-3 rounded-lg mt-10 hover:bg-indigo-500 transition ease-in-out duration-300">
